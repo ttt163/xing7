739d28decdd57754db984b553b23e5cdc4c08f95
tantingting <tantingting@xiaomi.com>
2018-11-01 12:45:16 +0800

    kuangjia

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+/**
+ * Author：zhoushuanglong
+ * Time：2017/7/27
+ * Description：root route
+ */
+
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+
+const rootRoutes = <div>
+    <Route path="/" getComponent={(nextState, callback) => {
+        require.ensure([], (require) => {
+            callback(null, require('../containers/Main').default)
+        }, 'Main')
+    }}>
+        <IndexRoute getComponent={(nextState, callback) => {
+            require.ensure([], (require) => {
+                callback(null, require('../containers/Enter').default)
+            }, 'Enter')
+        }}/>
+        <Route path='/enter' getComponent={(nextState, callback) => {
+            require.ensure([], (require) => {
+                callback(null, require('../containers/Enter').default)
+            }, 'Enter')
+        }}/>
+    </Route>
+</div>
+
+export default rootRoutes
