eaf0481a317ed527bcc56ba7e2858c2ebae7d57b
tantingting <tantingting@xiaomi.com>
2018-11-01 12:29:55 +0800

    框架

---
+++ src/containers/Enter/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * Author：zhoushuanglong
+ * Time：2017/7/26
+ * Description：enter
+ */
+
+import React, { Component } from 'react'
+import './index.scss'
+
+export default class Enter extends Component {
+    render () {
+        return <div className="game-wrap clearfix">enter</div>
+    }
+}
---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+export default class Main extends Component {
+    render () {
+        return <div>
+            1111233
+            {this.props.children}
+        </div>
+    }
+}
